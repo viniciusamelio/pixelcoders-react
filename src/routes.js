64a231a2a00de71510036e163bf355994ed32d08
Vinicius Amélio <vinicius.amelio@zoho.com>
2020-04-19 19:29:39 -0300

    Adding page 404

--- src/routes.js
+++ src/routes.js
@@ -6,21 +6,19 @@ import Main from './pages/main/main';
 import OrderView from './pages/order/order';
 import AboutView from './pages/about/about';
 import ServicesView from './pages/services/services';
+import NotFoundPage from './pages/404';
 
 const Routes = () => (
     <BrowserRouter>
         <Switch>
             <Route exact path="/" component={Main} />
-        </Switch>
-        <Switch>
             <Route path="/orcamento" component={OrderView} />
-        </Switch>
-        <Switch>
             <Route path="/sobre" component={AboutView} />
-        </Switch>
-        <Switch>
             <Route path="/servicos" component={ServicesView} />
-        </Switch>
+            <Route path="*"  component={NotFoundPage} />
+        </Switch>        
+        
+
     </BrowserRouter >
 );
 
